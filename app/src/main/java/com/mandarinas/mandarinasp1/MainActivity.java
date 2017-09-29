d2e0a2da8fe8c552a17ddba297ae4d3a71459d32
AlexBerni <alexbenacruz@gmail.com>
2017-09-29 08:08:24 +0200

    Tasty rana

--- app/src/main/java/com/mandarinas/mandarinasp1/MainActivity.java
+++ app/src/main/java/com/mandarinas/mandarinasp1/MainActivity.java
@@ -2,12 +2,31 @@ package com.mandarinas.mandarinasp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView plazatoros;
+    WebSettings webSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plazatoros = (WebView)findViewById(R.id.plazatoros);
+        webSettings = plazatoros.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        plazatoros.loadUrl("https://www.youtube.com/embed/7ynQKQzTK2g");
+        plazatoros.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public void onBackPressed() {
+        if(plazatoros.canGoBack()){
+            plazatoros.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
